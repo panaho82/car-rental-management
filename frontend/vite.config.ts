9b5f78b06f05282c703f6bdb01598fdae669382d
panaho82 <franck@MacBook-Pro-de-Galatea-Tech.local>
2024-12-15 15:36:48 -1000

    Add commonjs plugin and update Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import commonjs from '@rollup/plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    commonjs({
+      include: [/date-fns/, /node_modules/],
+      transformMixedEsModules: true
+    })
+  ],
   base: '/car-rental-management/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
+    }
   },
   build: {
     rollupOptions: {
